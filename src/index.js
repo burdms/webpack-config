c1229cc3115556f72f5ad3202ff34a396b3f141c
burdms <burdms@gmail.com>
2021-08-19 14:33:45 +0200

    It's working! Reloads browser tho (not injects)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import './assets/styles/style.scss';
 import './assets/scripts/main.js';
-
-// import './index.html';
-// import './test.html';
-
-// import './pug/pages/main.pug';
-// import './pug/pages/about.pug';
